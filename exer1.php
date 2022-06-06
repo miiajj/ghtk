bf77be0eaeb9efb1eb9721aeb8b5967d49e40c52
miiajj <84679284+miiajj@users.noreply.github.com>
2022-06-06 16:01:01 +0700

    push exercise1 - OOP/DI

---
+++ exer1.php
@@ -0,0 +1,80 @@
+<?php
+
+class Car implements Runnable
+{
+    protected $engine = null;
+    protected $wheel = null;
+
+    public function __construct(Engine $engine, Wheel $wheel) {
+        $this->engine = $engine;
+        $this->wheel = $wheel;
+    }
+
+    public function getEngine() {
+        return $this->engine;
+    }
+
+    public function getWheel() {
+        return $this->wheel;
+    }
+
+    public function setEngine(Engine $engine) {
+        $this->engine = $engine;
+    }
+
+    public function setWheel(Wheel $wheel) {
+        $this->wheel = $wheel;
+    }
+
+    public function run() {
+        echo "Car: <br>";
+        echo "- Engine type: " . $this->engine->getType() . "<br>";
+        echo "- Wheel type: " . $this->wheel->getType() . "<br>";
+        echo "Status: running...";
+    }
+
+}
+
+interface Runnable {
+    public function run();
+}
+
+class Engine
+{
+    protected $type;
+
+    public function __construct($type) {
+        $this->type = $type;
+    }
+
+    public function getType() {
+        return $this->type;
+    }
+
+    public function setType($type) {
+        $this->type = $type;
+    }
+}
+
+class Wheel
+{
+    protected $type;
+
+    public function __construct($type) {
+        $this->type = $type;
+    }
+
+    public function getType() {
+        return $this->type;
+    }
+
+    public function setType($type) {
+        $this->type = $type;
+    }
+}
+
+$engine = new Engine('promax');
+$wheel = new Wheel('promax');
+$car = new Car($engine, $wheel);
+
+$car->run();
